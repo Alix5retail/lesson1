a967ab6577744dddc3eda73a20df7a4a6f173fb0
alix5retail <alix5retail@gmail.com>
2020-09-08 23:12:01 +0300

    Определение "четности/нечетности", а также "положительное/негативное" ли число, вычленено в отдельные методы

--- src/lesson2/task6/Main.java
+++ src/lesson2/task6/Main.java
@@ -5,30 +5,48 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+
+    public static boolean isNumberEven = false;
+    public static boolean isNumberPositive = false;
+    public static boolean isNumberNegative = false;
+
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
         try {
             System.out.println("Введите число:");
             int inputNumber = Integer.parseInt(bufferedReader.readLine());
-            boolean isNumberEven = false;
+            checkIsNumberEven(inputNumber);
+            checkIsNumberPositive(inputNumber);
 
-            if (inputNumber % 2 == 0) {
-                isNumberEven = true;
-            }
-
-            if (inputNumber > 0 && isNumberEven) {
+            if (isNumberPositive && isNumberEven) {
                 System.out.println("Число положительное и четное: " + inputNumber);
-            } else if (inputNumber > 0) {
+            } else if (isNumberPositive) {
                 System.out.println("Число положительное и нечетное: " + inputNumber);
-            } else if (inputNumber < 0 && isNumberEven){
+            } else if (isNumberNegative && isNumberEven) {
                 System.out.println("Число отрицательное и четное: " + inputNumber);
-            } else if (inputNumber < 0){
+            } else if (isNumberNegative) {
                 System.out.println("Число отрицательное и нечетное: " + inputNumber);
             } else {
                 System.out.println("Число нулевое: " + inputNumber);
             }
+
         } catch (NumberFormatException e) {
             System.out.println("Допустимо вводить только цифры!");
+        }
+    }
+
+    static void checkIsNumberEven(int n) {
+        if (n % 2 == 0) {
+            isNumberEven = true;
+        }
+    }
+
+    static void checkIsNumberPositive(int n) {
+        if (n > 0) {
+            isNumberPositive = true;
+        } else if (n < 0) {
+            isNumberNegative = true;
         }
     }
 }
